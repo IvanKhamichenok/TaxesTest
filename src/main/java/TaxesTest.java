219c4d815eb9e9403e153f0ea9e8a0d117def347
IvanKhamichenok <iih2902@gmail.com>
2023-02-12 20:57:58 +0300

    TaxesTast

---
+++ src/main/java/TaxesTest.java
@@ -0,0 +1,34 @@
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class TaxesTest {
+
+
+    public double getTax(double income) {
+        if (income < 10000) {
+            return income * 0.13;
+        } else if (income >= 10000 && income < 50000) {
+            return income * 0.2;
+        } else {
+            return income * 0.3;
+        }
+    }
+
+
+    @Test(dataProvider = "incomeProvider")
+    public void checkTaxesCalculation(int income, double expTax) {
+        Assert.assertEquals(getTax(income), expTax * income, String.format("Taxes for %s should be %s", income, expTax));
+    }
+
+    @DataProvider
+    public Object[][] incomeProvider() {
+        return new Object[][]{
+                {0, 0},
+                {9999, 0.13},
+                {10000, 0.2},
+                {49999, 0.2},
+                {50000, 0.3}
+        };
+    }
+}
